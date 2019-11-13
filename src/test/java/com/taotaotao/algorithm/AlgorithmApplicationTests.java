d5f8f8104604db64c66a6dd755fce43f00bd03c3
humingtao <983086529@qq.com>
2019-11-13 20:57:02 +0800

    Initial commit

---
+++ src/test/java/com/taotaotao/algorithm/AlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taotaotao.algorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
